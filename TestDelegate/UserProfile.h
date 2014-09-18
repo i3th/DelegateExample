69175c94124a845fc556240d8c2867379d2d0a1e
Aliaksei Smuhaliou <smugalyov@gmail.com>
2014-09-18 23:21:29 +0300

    Add class UserProfile

---
+++ TestDelegate/UserProfile.h
@@ -0,0 +1,15 @@
+//
+//  UserProfile.h
+//  TestDelegate
+//
+//  Created by Aliaksei Smuhaliou on 9/18/14.
+//  Copyright (c) 2014 Aliaksei Smuhaliou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserProfile : NSObject
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSNumber *age;
+@property (nonatomic, strong) NSString *email;
+@end
---
+++ TestDelegate/UserProfile.m
@@ -0,0 +1,13 @@
+//
+//  UserProfile.m
+//  TestDelegate
+//
+//  Created by Aliaksei Smuhaliou on 9/18/14.
+//  Copyright (c) 2014 Aliaksei Smuhaliou. All rights reserved.
+//
+
+#import "UserProfile.h"
+
+@implementation UserProfile
+
+@end
