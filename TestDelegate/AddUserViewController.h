456971e16a928eda3184da4990894672300a6acf
Aliaksei Smuhaliou <smugalyov@gmail.com>
2014-09-19 08:59:10 +0300

    Add UI for Add User ViewController

---
+++ TestDelegate/AddUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddUserViewController.h
+//  TestDelegate
+//
+//  Created by Aliaksei Smuhaliou on 9/19/14.
+//  Copyright (c) 2014 Aliaksei Smuhaliou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddUserViewController : UIViewController
+
+@end
